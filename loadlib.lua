0a7d8864168007f602c22f681714ef1592a49181
Andrew McWatters <mcwattersandrew@gmail.com>
2016-11-17 11:55:12 -0700

    Changes to conform to Lua 5.1’s package library and loadlib implementation

--- loadlib.lua
+++ loadlib.lua
@@ -35,6 +35,44 @@ if ( love.system.getOS() == "Windows" ) then
 	package.handle = ffi.C.FindFirstChangeNotificationA( ".", true, 0x00000010 )
 end
 
+if ( not rawrequire ) then
+	rawrequire = require
+end
+
+local function getModuleFilename( modname )
+	local path = string.gsub( modname, "%.", "/" )
+	local filename = path .. ".lua"
+	if ( not love.filesystem.exists( filename ) ) then
+		filename = path .. "/init.lua"
+	end
+	return filename
+end
+
+function require( modname )
+	if ( package.watched[ modname ] ) then
+		return rawrequire( modname )
+	end
+
+	local status, ret = pcall( rawrequire, modname )
+	if ( not status ) then
+		error( ret, 2 )
+	end
+
+	local filename = getModuleFilename( modname )
+	package.watched[ modname ] = love.filesystem.getLastModified( filename )
+	return ret
+end
+
+local function unload( modname )
+	package.loaded[ modname ] = nil
+	package.watched[ modname ] = nil
+end
+
+function unrequire( modname )
+	unload( modname )
+	print( "Unloading " .. modname .. "..." )
+end
+
 package.watched = package.watched or {}
 
 local function reload( modname, filename )
