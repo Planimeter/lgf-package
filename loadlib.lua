ad19f42b8a26ac63df79dc42205176f5b0b0f9b2
Andrew McWatters <mcwattersandrew@gmail.com>
2017-06-24 16:11:34 -0700

    Update loadlib.lua

--- loadlib.lua
+++ loadlib.lua
@@ -12,11 +12,16 @@ end
 
 local function getModuleFilename( modname )
 	local module = string.gsub( modname, "%.", "/" )
-	for path in string.gmatch( package.path .. ";", "(.-);" ) do
-		path = string.gsub( path, "%./", "" )
-		local filename = string.gsub( path, "?", module )
-		if ( framework.filesystem.exists( filename ) ) then
-			return filename
+	for _, paths in ipairs( {
+		string.gmatch( package.path .. ";", "(.-);" ),
+		string.gmatch( love.filesystem.getRequirePath() .. ";", "(.-);" )
+	} ) do
+		for path in paths do
+			path = string.gsub( path, "%.[\\/]", "" )
+			local filename = string.gsub( path, "?", module )
+			if ( love.filesystem.exists( filename ) ) then
+				return filename
+			end
 		end
 	end
 end
@@ -33,7 +38,7 @@ function require( modname )
 
 	local filename = getModuleFilename( modname )
 	if ( filename ) then
-		local modtime, errormsg = framework.filesystem.getLastModified( filename )
+		local modtime, errormsg = love.filesystem.getLastModified( filename )
 		package.watched[ modname ] = modtime
 	end
 
@@ -63,18 +68,24 @@ local function reload( modname, filename )
 
 	print( err )
 
-	local modtime, errormsg = framework.filesystem.getLastModified( filename )
+	local modtime, errormsg = love.filesystem.getLastModified( filename )
 	package.watched[ modname ] = modtime
+end
+
+local function update( k, v )
+	local filename = getModuleFilename( k )
+	if ( not filename ) then
+		return
+	end
+
+	local modtime, errormsg = love.filesystem.getLastModified( filename )
+	if ( not errormsg and modtime ~= v ) then
+		reload( k, filename )
+	end
 end
 
 function package.update( dt )
 	for k, v in pairs( package.watched ) do
-		local filename = getModuleFilename( k )
-		if ( filename ) then
-			local modtime, errormsg = framework.filesystem.getLastModified( filename )
-			if ( not errormsg and modtime ~= v ) then
-				reload( k, filename )
-			end
-		end
+		update( k, v )
 	end
 end
